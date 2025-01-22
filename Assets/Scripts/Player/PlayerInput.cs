f626f9f4d155fbcecde299cc266358fe69810d5c
jvirkovskiy <jvirkovskiy@2reallife.com>
2025-01-22 21:18:18 +0300

    Movement controller

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -14,24 +14,35 @@ namespace Player
 		[SerializeField] private InputActionAsset _inputActionAsset;
 		[SerializeField, Range(0f, 0.99f)] private float _smoothing = 0.25f;
 		[SerializeField] private float _targetLerpSpeed = 1f;
+		[SerializeField] private Transform _lookAtTarget;
+		[SerializeField] private float _rotationSpeed = 1f;
 
 		private InputActionMap _inputActionMap;
 		private InputAction _movement;
+		private InputAction _rotation;
 		private NavMeshAgent _navMeshAgent;
 		private Vector3 _targetDirection;
 		private Vector3 _lastDirection;
 		private Vector3 _movementVector;
+		private Vector3 _rotationDelta;
 		private float _lerpTime;
-		private Transform _transform;
+		private Camera _camera;
 
 		private void Awake()
 		{
-			_transform = transform;
 			_navMeshAgent = GetComponent<NavMeshAgent>();
+
 			_inputActionMap = _inputActionAsset.FindActionMap("Navigation");
 			Assert.IsNotNull(_inputActionAsset, "Can't find action map Navigation.");
+
 			_movement = _inputActionMap.FindAction("Movement");
 			Assert.IsNotNull(_movement, "Can't find action map Movement.");
+
+			_rotation = _inputActionMap.FindAction("Rotation");
+			Assert.IsNotNull(_rotation, "Can't find action map Rotation.");
+
+			_camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+			Assert.IsNotNull(_camera, "Main Camera must have.");
 		}
 
 		private void Start()
@@ -40,7 +51,12 @@ namespace Player
 			Observable.FromEvent<InputAction.CallbackContext>(h => _movement.canceled += h, h => _movement.canceled -= h).Subscribe(OnMovementAction).AddTo(_disposables);
 			Observable.FromEvent<InputAction.CallbackContext>(h => _movement.performed += h, h => _movement.performed -= h).Subscribe(OnMovementAction).AddTo(_disposables);
 
+			Observable.FromEvent<InputAction.CallbackContext>(h => _rotation.started += h, h => _rotation.started -= h).Subscribe(OnRotationAction).AddTo(_disposables);
+			Observable.FromEvent<InputAction.CallbackContext>(h => _rotation.canceled += h, h => _rotation.canceled -= h).Subscribe(OnRotationAction).AddTo(_disposables);
+			Observable.FromEvent<InputAction.CallbackContext>(h => _rotation.performed += h, h => _rotation.performed -= h).Subscribe(OnRotationAction).AddTo(_disposables);
+
 			_movement.Enable();
+			_rotation.Enable();
 			_inputActionMap.Enable();
 			_inputActionAsset.Enable();
 		}
@@ -56,30 +72,35 @@ namespace Player
 			_movementVector = Vector3.Normalize(new Vector3(value.x, 0f, value.y));
 		}
 
+		private void OnRotationAction(InputAction.CallbackContext context)
+		{
+			var value = context.ReadValue<Vector2>();
+			_rotationDelta = _rotationSpeed * new Vector3(-value.y, value.x, 0);
+		}
+
 		private void Update()
 		{
-			if (_movementVector != _lastDirection)
-			{
-				_lerpTime = 0;
-			}
+			var tookAtTransform = _lookAtTarget.transform;
+			tookAtTransform.eulerAngles += _rotationDelta;
+			var lookAtRotation = tookAtTransform.rotation.eulerAngles.y;
+			var rotatedMovementVector = Quaternion.AngleAxis(lookAtRotation, Vector3.up) * _movementVector;
+			
+			// if (_movementVector != _lastDirection)
+			// {
+				// _lerpTime = 0;
+			// }
 
-			_lastDirection = _movementVector;
+			_lastDirection = rotatedMovementVector;
 			var interpolation = Mathf.Clamp01(_lerpTime * _targetLerpSpeed * (1f - _smoothing));
-			_targetDirection = Vector3.Lerp(_targetDirection, _movementVector, interpolation);
-			_navMeshAgent.Move(_targetDirection * _navMeshAgent.speed * Time.deltaTime);
-
-			var lookDirection = _movementVector;
-			if (lookDirection.sqrMagnitude > 0.00001f)
-			{
-				_transform.rotation = Quaternion.Lerp(_transform.rotation, Quaternion.LookRotation(lookDirection), interpolation);
-			}
-
+			_targetDirection = Vector3.Lerp(_targetDirection, rotatedMovementVector, interpolation);
+			_navMeshAgent.Move(_targetDirection * (_navMeshAgent.speed * Time.deltaTime));
 			_lerpTime += Time.deltaTime;
 		}
 
 		private void OnValidate()
 		{
 			Assert.IsNotNull(_inputActionAsset, "_inputActionAsset != null");
+			Assert.IsNotNull(_lookAtTarget, "_lookAtTarget != null");
 		}
 	}
 }
